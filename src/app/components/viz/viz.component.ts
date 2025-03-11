8c11eca83a3fba4b96ad50c41208129e469a1e16
Marty Trujillo <marty@ilikeemail.com>
2025-03-11 08:46:33 -0400

    Video 75 - moved visualization logic out of the component.

--- src/app/components/viz/viz.component.ts
+++ src/app/components/viz/viz.component.ts
@@ -7,12 +7,6 @@ import {
   ViewChild
 } from '@angular/core';
 
-import { StRenderer } from '../../interfaces/st';
-
-import { RendererService } from '../../services/three/renderer/renderer.service';
-import { StRendererService } from '../../services/st/renderer/st-renderer.service';
-
-import * as THREE from 'three';
 import { ComponentVisualizationService } from './visualization.service';
 
 @Component({
@@ -25,15 +19,11 @@ export class VizComponent implements AfterViewInit {
 
   stRendererInputId = input.required<number>();
 
-  rendererService: RendererService = inject(RendererService);
-  stRendererService: StRendererService = inject(StRendererService);
-
   componentVisualizationService: ComponentVisualizationService = inject(ComponentVisualizationService);
   
   @ViewChild('viz') rendererViewChild: ElementRef | undefined;
 
   ngAfterViewInit(): void {
-
     if (this.rendererViewChild?.nativeElement) {
       this.componentVisualizationService.renderInNativeElement(this.rendererViewChild, this.stRendererInputId());
     }
