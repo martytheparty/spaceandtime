b951c636e9286ac4d89e82a4231c3efb97f53a7f
Marty Trujillo <marty@ilikeemail.com>
2026-01-20 08:55:35 -0500

    Video 371 [code only] - Issue 9 - fixes multi-animation issue.

--- src/app/components/app-menu/app-menu.component.ts
+++ src/app/components/app-menu/app-menu.component.ts
@@ -14,6 +14,11 @@ import { UiService } from '../../services/ui/ui.service';
 import { AppModelService } from '../../services/appmodel/appmodel.service';
 import { ReflowType } from '../../interfaces/layout/reflow-types';
 import { CommonModule } from '@angular/common';
+import { AnimationService } from '../../services/entities/animations/animation.service';
+import { StAnimation, StMesh, StRenderer, StScene } from '../../interfaces/st';
+import { StRendererService } from '../../services/entities/st/renderer/st-renderer.service';
+import { StSceneService } from '../../services/entities/st/scene/st-scene.service';
+import { StMeshService } from '../../services/entities/st/mesh/st-mesh.service';
 
 
 @Component({
@@ -30,11 +35,16 @@ export class AppMenuComponent
   @ViewChild('addMenu') addMenuButton!: ElementRef<HTMLDivElement>;
 
   uiService: UiService = inject(UiService);
+  stRendererService: StRendererService = inject(StRendererService);
+  stSceneService: StSceneService = inject(StSceneService);
+  stMeshService : StMeshService = inject(StMeshService);
   appModelService: AppModelService = inject(AppModelService);
   injector: Injector = inject(Injector);
+  animationService: AnimationService = inject(AnimationService);
 
   isOpen = false;
   reflow: ReflowType = this.appModelService.getReflow();
+  addCount = 0;
 
 
   closeHandler(): boolean {
@@ -75,6 +85,26 @@ export class AppMenuComponent
   addVisualization(): number
   {
     const stId = this.uiService.addRenderer();
+    // add default animation to the renderer
+
+    // step 1 create an st animation
+    const animation: StAnimation = this.animationService.createAnimationForCount(this.addCount++);
+
+    // const animation = this.a
+    // step 2 get the Mesh
+
+    const stRenderer: StRenderer = this.stRendererService.getRendererById(stId);
+    
+    const stScene: StScene = this.stSceneService.getSceneById(stRenderer.stSceneId);
+
+    // assumes that there is only one mesh that should be automatically animated
+
+    const stMeshId = stScene.stMeshIds[0];
+    const stMesh: StMesh = this.stMeshService.getMeshById(stMeshId);
+
+    // step 3 add the animation to the Mesh
+
+    stMesh.stAnimations.push(animation);
     return stId;
   }
  
